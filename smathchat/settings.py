23572a06fe113f71302d9b0ddeaf607f6b250f96
ZoomLee <ZoomLee@users.noreply.github.com>
2021-05-10 07:47:11 -0700

    Update settings.py

--- smathchat/settings.py
+++ smathchat/settings.py
@@ -143,6 +143,10 @@ CHANNEL_LAYERS = {
     },
 }
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Configure Django App for Heroku.
 #import django_heroku
