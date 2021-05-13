2d609301914529a134f7314f257d087c880d456f
ZoomLee <ZoomLee@users.noreply.github.com>
2021-05-13 10:45:03 -0700

    Update routing.py

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
+    url(r'ws/chat/addition1/$', consumers.ChatConsumer),
     url(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
 ]
