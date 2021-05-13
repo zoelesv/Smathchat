2b2d2172dcb3a681a39af61f1fdb67c8a89494ef
ZoomLee <ZoomLee@users.noreply.github.com>
2021-05-13 09:46:55 -0700

    update wss route

--- smathchat/routing.py
+++ smathchat/routing.py
@@ -1,12 +1,13 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 import chat.routing
 
 
 application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
+    'websocket':  AllowedHostsOriginValidator(AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
-    ),
+    )),
 })
